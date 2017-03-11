16577dc09a44b5c29444f35fc0f9ca4180cb2545
mycomputericons <mycomputericons@gmail.com>
2017-03-11 09:38:28 +0100

    Added basic project

---
+++ src/main/java/com/exercise/BasicSpringApplication.java
@@ -0,0 +1,12 @@
+package com.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringApplication.class, args);
+	}
+}
